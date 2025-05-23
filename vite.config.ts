8e6831622113929e607f8de932d5c4a0ff0e0dd9
Joao Vitor <jovms.2004@gmail.com>
2025-05-22 21:58:03 -0300

    Configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/Projeto_Faculdade_Uninove",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
